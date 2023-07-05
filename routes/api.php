fd7a0c4ffaaf1d70f8b9e82867a7a31b5ea8b0a7
Gierdiaz <gierdiaz@hotmail.com>
2023-07-05 11:58:03 -0300

    Autenticação com Laravel Sanctum

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,7 @@ use App\Http\Controllers\Api\V1\InvoiceController;
 Route::prefix('v1')->group(function(){
     Route::get('/users', [UserController::class, 'index']);
     Route::get('/users/{user}', [UserController::class, 'show']);
+    Route::get('/login', [AuthController::class, 'login']);
     
     Route::get('/invoices', [InvoiceController::class, 'index']);
     Route::get('/invoices/{invoice}', [InvoiceController::class, 'show']);
